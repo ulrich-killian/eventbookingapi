9cf03d56f6e879d39210e72c467f6b6061eb6a84
79Rcf <197534687+79Rcf@users.noreply.github.com>
2026-05-05 11:51:49 +0100

    update db config to use DATABASE_URL for production

--- src/schema/db.js
+++ src/schema/db.js
@@ -1,30 +1,32 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const { Pool } = pg;
+const { Pool } = pg; 
 
-const developmentConfig =({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
-
-const pool = new Pool( developmentConfig );
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? { 
+        connectionString: process.env.DATABASE_URL, 
+        ssl: { rejectUnauthorized: false } 
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_NAME,
+      }
+);
 
 async function testConnection() {
   try {
     const client = await pool.connect();
     client.release();
-    console.log("postgres db connected succeffuly connected")
+    console.log("postgres db connected successfully");
   } catch (err) {
     console.error("Error connecting to the database:", err);
   }
 }
-
-
 
 export { pool, testConnection };
